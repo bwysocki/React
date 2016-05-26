2f3d612284b8c1cd2ad082687d1fa5f69e6fe023
Bartosz <wysockibartosz@gmail.com>
2016-05-26 14:39:49 +0200

    Lifecycle.

--- app.js
+++ app.js
@@ -1,6 +1,57 @@
-let h1 = React.createElement('h1', {}, 'Books'),
-	li = React.createElement('li', {}, 'Przewodnik po Madagaskarze'),
-	ul = React.createElement('ul', {}, li),
-	div = React.createElement('div', {}, h1, ul)
+let BooksHeader = React.createClass({
+	render : function() {
+		return React.createElement('h1', {
+			style : {
+				color : this.props.color
+			}
+		}, this.props.children); //className i htmlFor
+	}
+});
 
-ReactDOM.render(div, document.getElementById('react-element'));
+let BooksList = React.createClass({
+	render : function() { //lifecycle - generate DOM representation
+		let lookListItemNode = this.props.books.map(function (book, idx){
+			return React.createElement('li', {
+				id: 'book-' + idx, 
+				key: idx
+			}, book.title, React.createElement('span', {}, ' ' + book.year)); 
+		});
+		
+		return React.createElement('ul', {}, lookListItemNode);
+	}
+});
+
+var BooksComponent = React.createClass({
+	loadBooks : function() {
+		var bookList = [ {
+			title : 'Przewodnik po Madagaskarze',
+			year : 2014
+		}, {
+			title : 'Encyklopedia kretów',
+			year : 2011
+		} ];
+		this.setState({
+			books : bookList
+		})
+	},
+	getInitialState : function() {
+		return {
+			books : []
+		}
+	},
+	render : function() {
+		return React.createElement('div', {
+			className : 'books-component'
+		}, React.createElement(BooksHeader, {
+			color : 'blue'
+		}, 'Books'), React.createElement(BooksList, {
+			books : this.state.books
+		}));
+	},
+	componentDidMount : function() {
+		this.loadBooks();
+	}
+});
+
+var element = React.createElement(BooksComponent);
+ReactDOM.render(element, document.getElementById('react-element'));
